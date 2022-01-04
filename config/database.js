88de47c1174538c1ea9abdfa8e497a2a1c16187d
laluutruongsa <63277873+laluutruongsa@users.noreply.github.com>
2022-01-04 21:13:14 +0700

    first commit

---
+++ config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+	'url': 'mongodb://localhost:27017',
+	'options': {
+		//'user':   'root',
+		//'pass':   '123',
+		'dbName': 'server',
+		'useNewUrlParser': true,
+		'useUnifiedTopology': true,
+		//'autoIndex':       false,
+	},
+};

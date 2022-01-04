91012da2ea1ca089232208cf91358bd773f7dc69
laluutruongsa <63277873+laluutruongsa@users.noreply.github.com>
2022-01-05 01:33:53 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-	'url': 'mongodb://root:123@cluster0-shard-00-00.e70td.mongodb.net:27017,cluster0-shard-00-01.e70td.mongodb.net:27017,cluster0-shard-00-02.e70td.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-14avsz-shard-0&authSource=admin&retryWrites=true&w=majority',
+	'url': 'mongodb+srv://root:<password>@cluster0.e70td.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 	'options': {
 		//'user':   'root',
 		//'pass':   '123',

fc2390939a83380a06a383877c7d026862262094
laluutruongsa <63277873+laluutruongsa@users.noreply.github.com>
2022-01-04 22:31:00 +0700

    first commit

--- config/database.js
+++ config/database.js
@@ -1,11 +1,7 @@
 module.exports = {
-	'url': 'mongodb://localhost:27017',
+	'url': 'mongodb://root:123@cluster0-shard-00-00.e70td.mongodb.net:27017,cluster0-shard-00-01.e70td.mongodb.net:27017,cluster0-shard-00-02.e70td.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-14avsz-shard-0&authSource=admin&retryWrites=true&w=majority',
 	'options': {
 		//'user':   'root',
 		//'pass':   '123',
-		'dbName': 'server',
-		'useNewUrlParser': true,
-		'useUnifiedTopology': true,
-		//'autoIndex':       false,
 	},
 };
